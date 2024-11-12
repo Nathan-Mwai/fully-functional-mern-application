97a60605f7effdcd1adddd0e2ea359866920e976
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-12 23:33:11 +0300

    Created a separate folder(controllers) that will control all the functions that are to take place

---
+++ README.md
@@ -0,0 +1,3 @@
+<!-- BACKEND FOLDER -->
+- routes folder defines the routes
+- controllers folder is used to create the function for the routes
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,16 +1,13 @@
 import  express from 'express'
+import { login, logout, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 //set up routes
-router.get('/signup', (req, res) => {
-    res.send('Signup route');
-});
-router.get('/login', (req, res) => {
-    res.send('Login route');
-});
-router.get('/logout', (req, res) => {
-    res.send('Logout route!');
-});
+router.get('/signup', signup);
+
+router.get('/login', login);
+
+router.get('/logout',logout);
 
 export default router;
