bc4f0dcbdce1c81293b9186f8bae922d7d42c1af
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-13 00:11:39 +0300

    Ok now creating a part for verification code

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,29 +1,33 @@
-import { User } from '../models/user.model.js'
-import bcrypt from 'bcryptjs'
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
-export const signup = async (req,res) => {
-    const {email, password, name} = req.body;
-    try {
-        if(!email || !password || !name) {
-            throw new Error("Please provide all fields")
-        }
-
-        const userAlreadyExists = await User.findOne({email}) 
-        if(userAlreadyExists) {
-            throw new Error("User already exists")
-        }
-
-        // const hashedPassword = await bcrypt.hash(password, 10)
-
-    } catch (error) {
-        res.status(400).json({success:false , message: error.message})
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    if (!email || !password || !name) {
+      throw new Error("Please provide all fields");
     }
-}
 
-export const login = async (req,res) => {
-    res.send("login route")
-}
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+    //This is the password spot
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
 
-export const logout = async (req,res) => {
-    res.send("Logout route")
-}
+export const login = async (req, res) => {
+  res.send("login route");
+};
+
+export const logout = async (req, res) => {
+  res.send("Logout route");
+};
