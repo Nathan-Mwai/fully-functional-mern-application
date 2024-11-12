f457ff489aa228147b41c00946033fa3e001e82a
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-12 23:42:29 +0300

    Created the initial schema for the users

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 <!-- BACKEND FOLDER -->
 - routes folder defines the routes
 - controllers folder is used to create the function for the routes
+- from the user models, timestamps gives me the created at and updated at fields
---
+++ backend/models/user.model.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    name: {type: String, required: true},
+    lastlogin:{type: Date, default:Date.now},
+    isVerified: {type: Boolean, default: false}
+},{timestamps:true})
