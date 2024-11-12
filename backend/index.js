45a3611ef42edf6036c81d3d7cac8b86e79015b2
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-12 23:18:10 +0300

    Defining the initial routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { connectDB } from './db/connectDB.js'
 import dotenv from 'dotenv'
+import authRoutes from './routes/auth.route.js'
+
 dotenv.config()
 
 const app = express()
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,4 +2,15 @@ import  express from 'express'
 
 const router = express.Router();
 
+//set up routes
+router.get('/signup', (req, res) => {
+    res.send('Signup route');
+});
+router.get('/login', (req, res) => {
+    res.send('Login route');
+});
+router.get('/logout', (req, res) => {
+    res.send('Logout route!');
+});
+
 export default router;
