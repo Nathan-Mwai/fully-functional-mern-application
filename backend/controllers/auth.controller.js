e422f5ce144747350812462ea2a43809751b9601
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-13 16:54:05 +0300

    Have set up the reset password logic

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -156,3 +156,37 @@ export const forgotPassword = async(req,res)=> {
     
   }
 }
+
+export const resetPassword = async (req,res) => {
+  try {
+    const {token} = req.params;
+    const {password} = req.body;
+
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpiresAt: {$gt: Date.now()}
+    })
+
+    if (!user){
+      return res.status(400).json({success: false, message: "Invalid or expired token"})
+    }
+
+    // update password
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user.password = hashedPassword;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpiresAt = undefined;
+    await User.save()
+
+    await sendResetSuccessEmail(user.email)
+
+    res.status(200).json({
+      success: true,
+      message: "Password reset successfully"
+    })
+  } catch (error) {
+    console.log("Error in resetPassword", error);
+    res.status(400).json({success: false, message: error.message})
+  }
+}
